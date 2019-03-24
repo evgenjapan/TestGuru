674be8c7d734c22dc20b10e1f9e54e664f32912f
evgenjapan <catfat19@gmail.com>
2019-03-24 23:40:30 +0300

    initial lesson4

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,7 @@
 class Test < ApplicationRecord
   belongs_to :category
+
+  def self.titles_by_category(category_title)
+    Category.find_by_title(category_title).order(title: :desc).pluck(:title)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+initial_categories = Category.create([{title: 'Web Development'}, {title: 'Programming languages'}])
+
+initial_tests = Test.create([
+                                {title: 'Rails', level: 0, category: initial_categories[0]},
+                                {title: 'Ruby', level: 0, category: initial_categories[1]},
+                            ])
+initial_questions = Question.create([
+                                        {body: 'Who?', test: initial_tests[0]},
+                                        {body: 'Why?', test: initial_tests[1]},
+                                    ])
+initial_answers = Answer.create([
+                                    {body: 'Me', correct: true, question: initial_questions[0]},
+                                    {body: 'Just because', correct: true, question: initial_questions[1]}
+                                ])
+admin = User.create({username: 'admin', password: 'root', email: 'admin@admin.com'})
