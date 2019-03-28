c31146076c9cdb68829b9248d5efc66ef2e2555a
evgenjapan <catfat19@gmail.com>
2019-03-28 23:39:47 +0300

    Lesson 6

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,8 @@
 class Question < ApplicationRecord
-  has_many :answer
+  has_many :answers
   belongs_to :test
+
+  validates :body, presence: true
+  validates :answers, length: { in: 0..4 }
+
 end
