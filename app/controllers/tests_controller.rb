f5b3e223b996214a813730f0915be2891be8e285
evgenjapan <catfat19@gmail.com>
2019-04-30 22:03:55 +0300

    Lesson11

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,6 +1,5 @@
 class TestsController < ApplicationController
   before_action :get_test, only: %i[show destroy edit update start]
-  before_action :set_user, only: %i[start new create]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_from_not_found
 
@@ -8,58 +7,15 @@ class TestsController < ApplicationController
     @tests = Test.all
   end
 
-  def destroy
-    @test.destroy
-    redirect_to action: :index
-  end
-
-  def show
-  end
-
-  def edit
-    @categories = Category.all
-  end
-
-  def new
-    @test = Test.new
-    @categories = Category.all
-  end
-
-  def create
-    @test = @user.tests.new(test_params)
-
-    if @test.save
-      redirect_to @test
-    else
-      render :new
-    end
-  end
-
-  def update
-    if @test.update(test_params)
-      redirect_to @test
-    else
-      render :edit
-    end
-  end
-
   def start
-    @user.tests.push(@test)
-    redirect_to @user.test_passage(@test)
+    current_user.tests.push(@test)
+    redirect_to current_user.test_passage(@test)
   end
 
   private
 
   def get_test
     @test = Test.find(params[:id])
-  end
-
-  def set_user
-    @user = current_user
-  end
-
-  def test_params
-    params.require(:test).permit(:title, :level, :category_id)
   end
 
   def rescue_from_not_found
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   VALID_EMAIL_PATTERN = /\A\w+@\w+\.\w+\z/
 
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable,
+         :confirmable
+         #:trackable
+
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :created_tests, class_name: 'Test', foreign_key: :author_id
@@ -8,8 +16,6 @@ class User < ApplicationRecord
   validates :email, presence: true, format: VALID_EMAIL_PATTERN, uniqueness: true
 
   scope :test_by_level, -> (level) { joins(:tests).where(tests:{level: level}) }
-
-  has_secure_password
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
